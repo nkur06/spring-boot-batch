ac5adaabb15754396af373012380d11470da60a4
nkur06 <38080559+nkur06@users.noreply.github.com>
2022-12-08 23:36:50 +0530

    Spring Batch Changes Done

---
+++ src/main/java/com/javainuse/DemoCLI.java
@@ -0,0 +1,20 @@
+//package com.javainuse;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+//import org.springframework.scheduling.annotation.EnableScheduling;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+//@EnableScheduling
+//@RestController
+//public class DemoCLI {
+//    @RequestMapping("/get-message")
+//    String greetHello() {
+//         "Hi ! This is a message from groovy script";
+//
+//    }
+//
+//
+//}
